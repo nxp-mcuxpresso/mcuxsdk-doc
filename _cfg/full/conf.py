98f4e6b9e904d721c5cb7d16e59e6f5a00c7adf9
Susan Su <susan.su@nxp.com>
2024-06-24 11:07:47 +0800

    cfg: change navigation depth to 5
    Signed-off-by: Susan Su <susan.su@nxp.com>

--- _cfg/full/conf.py
+++ _cfg/full/conf.py
@@ -185,6 +185,7 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
     "logo_only": True,
+    'navigation_depth': 5,
     "prev_next_buttons_location": None
 }
 html_baseurl = "https://docs.zephyrproject.org/latest/"
