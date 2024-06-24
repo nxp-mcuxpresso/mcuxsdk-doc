5f0ced7350e55a56499ba512fb329caf032a0dc7
Susan Su <susan.su@nxp.com>
2024-06-24 16:13:36 +0800

    cfg: fix _template path issue
    Signed-off-by: Susan Su <susan.su@nxp.com>

--- _cfg/nodoxygen/conf.py
+++ _cfg/nodoxygen/conf.py
@@ -161,7 +161,7 @@ breathe_separate_member_pages = True
 # highlight_language = 'c'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['../../_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
